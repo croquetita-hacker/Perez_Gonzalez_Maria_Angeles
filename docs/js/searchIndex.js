67cacc9617932bdcd3c982d63c68545e0374aeca
Croquetita Hacker <angelespergon@gmail.com>
2025-05-25 19:45:20 +0200

    documentacion corregida

--- docs/js/searchIndex.js
+++ docs/js/searchIndex.js
@@ -1,4 +1,14 @@
 Search.appendIndex(
     [
-            ]
+                {
+            "fqsen": "\\compararPalindromos\u0028\u0029",
+            "name": "compararPalindromos",
+            "summary": "Compara\u0020si\u0020dos\u0020palabras\u0020son\u0020pal\u00EDndromos",
+            "url": "namespaces/default.html#function_compararPalindromos"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
 );
